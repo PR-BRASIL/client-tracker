a61ad8029d6408a27b42fbc3cfd9ee28de617129
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2024-01-28 02:04:33 -0300

    chore: remove console.log

--- src/presentation/event/new-player-profile-log.ts
+++ src/presentation/event/new-player-profile-log.ts
@@ -3,7 +3,6 @@ import { socket } from "../../main/config/socket";
 
 export const newPlayerProfileLogEvent = async (path: string) => {
   const lastLine = (await readLastLines.read(path, 1)).trim();
-  console.log(lastLine);
 
   socket.emit("newPlayerProfileLog", lastLine);
 };
