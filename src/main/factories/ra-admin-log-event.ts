a7e13562f71bc290f5c9d08b229533150c7794a4
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2024-01-31 00:03:00 -0300

    fix: report log is showing in two events

--- src/main/factories/ra-admin-log-event.ts
+++ src/main/factories/ra-admin-log-event.ts
@@ -1,7 +1,7 @@
 import { LogEventDecorator } from "../../decorators/log";
 import { RaAdminLogEvent } from "../../presentation/event/ra-admin-log";
 
-export const makeRaAdminLogEvent = (): RaAdminLogEvent => {
+export const makeRaAdminLogEvent = () => {
   const event = new RaAdminLogEvent();
 
   return new LogEventDecorator(event);
--- src/presentation/event/ra-admin-log.ts
+++ src/presentation/event/ra-admin-log.ts
@@ -1,20 +1,28 @@
 import readLastLines from "read-last-lines";
 import { socket } from "../../main/config/socket";
 import type { Event } from "../protocols/event";
+import { logger } from "../../utils/logger";
 
 export class RaAdminLogEvent implements Event {
   public async handle(path: string): Promise<void> {
     const MAX_LINE = 1;
     const lastLine = (await readLastLines.read(path, MAX_LINE)).trim();
 
-    socket.emit("adminLog", lastLine);
-
-    const COMMAND_INDEX = 2;
-    const lastLineCommand = lastLine.split(" ")[COMMAND_INDEX];
-    switch (lastLineCommand.toLowerCase()) {
-      case "!report":
-        socket.emit("reportLog", lastLine);
-        break;
+    if (this.isReportLog(lastLine)) {
+      socket.emit("reportLog", lastLine);
+      return;
     }
+
+    socket.emit("adminLog", lastLine);
+  }
+
+  private isReportLog(message: string): boolean {
+    const reportText = "!REPORT";
+    const isReportLog = message.includes(reportText);
+
+    if (!isReportLog) return false;
+
+    logger.debug("Report log executed");
+    return true;
   }
 }
