09478ba83b9b132df246dfeebe6da4a3d196d41a
willian <willian.cacoelho@gmail.com>
2024-02-04 18:53:01 -0300

    feat: game state updated

--- src/presentation/event/game-state.ts
+++ src/presentation/event/game-state.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import type { Event } from "../protocols/event";
 import { socket } from "../../main/config/socket";
+import { logger } from "../../utils/logger";
 
 export class GameStateEvent implements Event {
   public async handle(): Promise<void> {
@@ -8,12 +9,13 @@ export class GameStateEvent implements Event {
       "https://servers.realitymod.com/api/Serverinfo"
     );
 
-    socket.emit(
-      "gameState",
-      data.servers.find(
-        (server: { serverId: string }) =>
-          server.serverId == "3cc9f2cf9ca951d98891eea56ccb0e4c7cfcfb85"
-      )
+    const serverInfo = data.servers.find(
+      (server: { serverId: string }) =>
+        // server.serverId == "3cc9f2cf9ca951d98891eea56ccb0e4c7cfcfb85"
+        server.serverId == "3cc9f2cf9ca951d98891eea56ccb0e4c7cfcfb85"
     );
+    logger.debug("serverinfo executed");
+
+    socket.emit("gameState", serverInfo);
   }
 }
