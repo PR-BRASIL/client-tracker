5ae62e23b2d54e6ae3f7471cb408966eeb23835c
willian <willian.cacoelho@gmail.com>
2025-03-20 18:45:09 -0300

    aa

--- src/main/server.ts
+++ src/main/server.ts
@@ -11,11 +11,13 @@ import fs from "fs";
 import { makeGameLogEvent } from "./factories/game-log-event";
 import { makeGameStateEvent } from "./factories/game-state-event";
 import { env } from "./config/env";
+import path from "path";  // Importando path para garantir caminhos corretos
+
 config();
 
 const chokidarOptions: WatchOptions = {
   usePolling: true,
-  interval: 100,
+  interval: 100,  // Ajustando intervalo para polling
 };
 
 const adminLogPath = env.adminLogPath;
@@ -45,8 +47,10 @@ const watcherGameLog = chokidar.watch(gameLogPath, {
   persistent: true,
   ...chokidarOptions,
 });
+
 const processedFiles: Set<string> = new Set();
 
+// Lendo o diretório do log de jogos e registrando os arquivos processados
 fs.readdir(gameLogPath, (err, files) => {
   if (err) {
     logger.error("Erro ao ler o diretório:", err);
@@ -54,16 +58,33 @@ fs.readdir(gameLogPath, (err, files) => {
   }
 
   files.forEach((file) => {
-    processedFiles.add(`${gameLogPath}${file}`);
+    const fullFilePath = path.join(gameLogPath, file);  // Usando path.join para garantir o caminho correto
+    processedFiles.add(fullFilePath);
   });
 });
 
+// Observando a adição de novos arquivos no log de jogos
 watcherGameLog.on("add", async (path) => {
+  let fileReady = false;
+
+  // Por exemplo, verificando se o arquivo tem tamanho maior que 0 bytes
+  while (!fileReady) {
+    const stats = await fs.promises.stat(path);
+    if (stats.size > 0) {
+      fileReady = true;
+    } else {
+      // Se o arquivo ainda não estiver pronto, aguarde um pouco e tente novamente
+      await new Promise((resolve) => setTimeout(resolve, 500));  // Aguarda 500ms
+    }
+  }
+
+  console.log("Arquivo pronto para ser processado:", path);
   if (processedFiles.has(path)) return;
 
   await makeGameLogEvent().handle(path);
 });
 
+// Definindo um intervalo para o evento de estado do jogo
 // eslint-disable-next-line @typescript-eslint/no-magic-numbers
 const ONE_MINUTE = 60 * 1000;
 setInterval(makeGameStateEvent().handle, ONE_MINUTE);
