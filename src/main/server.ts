9dd5159f6b30bb22db918a3f1b88612aea58ea14
willian <willian.cacoelho@gmail.com>
2025-03-21 11:06:19 -0300

    feat: change

--- src/main/server.ts
+++ src/main/server.ts
@@ -11,13 +11,13 @@ import fs from "fs";
 import { makeGameLogEvent } from "./factories/game-log-event";
 import { makeGameStateEvent } from "./factories/game-state-event";
 import { env } from "./config/env";
-import path from "path";  // Importando path para garantir caminhos corretos
+import path from "path"; // Importando path para garantir caminhos corretos
 
 config();
 
 const chokidarOptions: WatchOptions = {
   usePolling: true,
-  interval: 100,  // Ajustando intervalo para polling
+  interval: 100, // Ajustando intervalo para polling
 };
 
 const adminLogPath = env.adminLogPath;
@@ -45,52 +45,12 @@ watcherChatLog.on("change", (path) => makeChatLogEvent().handle(path));
 const gameLogPath = env.gameLogPath;
 const watcherGameLog = chokidar.watch(gameLogPath, {
   persistent: true,
+  ignoreInitial: true,
   ...chokidarOptions,
 });
 
-const processedFiles: Set<string> = new Set();
-
-// Lendo o diretório do log de jogos e registrando os arquivos processados
-fs.readdir(gameLogPath, (err, files) => {
-  if (err) {
-    logger.error("Erro ao ler o diretório:", err);
-    return;
-  }
-
-  files.forEach((file) => {
-    const fullFilePath = path.join(gameLogPath, file);  // Usando path.join para garantir o caminho correto
-    processedFiles.add(fullFilePath);
-  });
-
-  watcherGameLog.on("add", async (path) => {
-    let fileReady = false;
-  
-    // Por exemplo, verificando se o arquivo tem tamanho maior que 0 bytes
-    while (!fileReady) {
-      const stats = await fs.promises.stat(path);
-      if (stats.size > 0) {
-        fileReady = true;
-      } else {
-        // Se o arquivo ainda não estiver pronto, aguarde um pouco e tente novamente
-        await new Promise((resolve) => setTimeout(resolve, 500));  // Aguarda 500ms
-      }
-    }
-  
-    console.log("Arquivo pronto para ser processado:", path);
-    if (processedFiles.has(path)) return;
-
-    console.log("processando arquivo", path);
-  
-    await makeGameLogEvent().handle(path);
-  });
+watcherGameLog.on("add", async (path) => {
+  await makeGameLogEvent().handle(path);
 });
-
-// Observando a adição de novos arquivos no log de jogos
-
-
-// Definindo um intervalo para o evento de estado do jogo
-// eslint-disable-next-line @typescript-eslint/no-magic-numbers
-const ONE_MINUTE = 60 * 1000;
-setInterval(makeGameStateEvent().handle, ONE_MINUTE);
 
 logger.info("Client has been started!");
